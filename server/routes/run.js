b17d920a828205b26fdb3eb1e66081810b249786
JSSaini07 <mr.jssaini07@gmail.com>
2016-07-30 12:53:44 +0530

    catch server error

--- server/routes/run.js
+++ server/routes/run.js
@@ -8,31 +8,33 @@ router.get('/run',function(req,res){
   language=req.query.lang;
   code=req.query.code;
   input=req.query.input;
-  request({
-    url: RUN_URL,
-    method: "POST",
-    form:{
-      'client_secret': CLIENT_SECRET,
-      'async': 0,
-      'input':input,
-      'source': code,
-      'lang': language,
-      'time_limit': 5,
-      'memory_limit': 262144,
-    }
-  },function(error,response,body) {
-    code_id=JSON.parse(body).code_id;
-    output=JSON.parse(body);
-    date=new Date();
-    username="";
-    if(req.session.username!=undefined)
-    {
-        username=req.session.username;
-    }
-    var r=new codes({code_id:code_id,username:username,date:date,lang:language,code:code,parameters:input,output:output});
-    r.save();
-    res.end(body);
-  });
+    request({
+      url: RUN_URL,
+      method: "POST",
+      form:{
+        'client_secret': CLIENT_SECRET,
+        'async': 0,
+        'input':input,
+        'source': code,
+        'lang': language,
+        'time_limit': 5,
+        'memory_limit': 262144,
+      }
+    },function(error,response,body) {
+      try{
+        code_id=JSON.parse(body).code_id;
+        output=JSON.parse(body);
+        date=new Date();
+        username="";
+        if(req.session.username!=undefined)
+        {
+            username=req.session.username;
+        }
+        var r=new codes({code_id:code_id,username:username,date:date,lang:language,code:code,parameters:input,output:output});
+        r.save();
+        res.end(body);
+      }catch(e){}
+    });
 });
 
 module.exports=router;
